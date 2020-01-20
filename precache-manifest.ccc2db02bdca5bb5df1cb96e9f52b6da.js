7b08b67ede2ffd50247f6fe0860612af928fdbdb
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-01-20 11:58:08 +0700

    Updates

---
+++ precache-manifest.ccc2db02bdca5bb5df1cb96e9f52b6da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4342ccac771ccef844ae8ce2c29e57",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a4e9efb9fbb979956aea",
+    "url": "/monsters-rolodex/static/css/main.2a48b914.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4e9efb9fbb979956aea",
+    "url": "/monsters-rolodex/static/js/main.b2f55206.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
